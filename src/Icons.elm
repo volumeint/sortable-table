ac8ba67110242ae07868b80ec0666becaf10a272
Nate Gibbons <ngibbons@volumeintegration.com>
2018-05-10 06:24:36 -0600

    removed debug statement

--- src/Icons.elm
+++ src/Icons.elm
@@ -16,7 +16,7 @@ svgBox =
 
 upArrow : List (Html.Attribute msg) -> Html msg
 upArrow customAttrs =
-    svg (Debug.log "custom" customAttrs ++ svgBox)
+    svg (customAttrs ++ svgBox)
         [ path [ d "M0 0h24v24H0V0z", fill "none" ] []
         , path [ d "M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z" ] []
         ]
